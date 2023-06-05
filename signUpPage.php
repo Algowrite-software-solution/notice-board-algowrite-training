e8487155e2d1fc84526d17972816381209450adc
VirajKelum <workmail12311@gmail.com>
2023-06-05 12:33:36 +0530

    Update signUpPage.php

--- signUpPage.php
+++ signUpPage.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notice Board | SIGNUP |</title>
-    <link rel="stylesheet" href="../css/bootstrap.css" />
-    <link rel="stylesheet" href="../css/styles.css" />
+    <link rel="stylesheet" href=".../css//bootstrap.css" />
+    <link rel="stylesheet" href=".../css//style.css" />
 </head>
 
 <body  style="background-color: #5d36cb;background-image: linear-gradient(65deg,#ffffff 50%,#d95cb1 50%);">
