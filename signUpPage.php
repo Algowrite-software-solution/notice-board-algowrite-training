042642b0666099e8cca8eafd291115d876245721
kavindu202 <kavindu.sasanka962@gmail.com>
2023-06-07 00:30:08 +0530

    signUp frontend done

--- signUpPage.php
+++ signUpPage.php
@@ -3,14 +3,34 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <title>SignUp Page</title>
 </head>
 
 <body>
-    sign in <br>
-    sign up
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="row signUpContainer">
+                    <div class="col-8 p-5 rounded" style="background-color:gray;">
+                      <div class="row d-flex align-items-center justify-content-center">
+                        <div class="col-9">
+                            <h3 class="text-center fw-bold">Sign Up </h3>
+                            <div class="mt-4 pb-4"><input class="form-control" type="email" placeholder="email"/></div>
+                            <div class="pb-4"><input class="form-control" type="password" placeholder="passowrd"/></div>
+                            <div class="pb-4"><input class="form-control" type="password" placeholder="confirm-password"/></div>
+                            <hr/>
+                            <div class="d-grid pb-2"><button class="btn btn-primary">Sign Up</button></div>
+                            <div class="d-grid"><button class="btn btn-primary">Sign In</button></div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
