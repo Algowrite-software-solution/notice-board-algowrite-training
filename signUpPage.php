60336442f650dc9950bbfa1077bb74e5f3facb81
kavindu202 <kavindu.sasanka962@gmail.com>
2023-06-08 00:21:49 +0530

    done admin signUp

--- signUpPage.php
+++ signUpPage.php
@@ -14,16 +14,16 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 bg-black">
                 <div class="row signUpContainer">
-                    <div class="col-8 p-5 rounded" style="background-color:gray;">
+                    <div class="col-8 p-5 rounded-5 shadow" style="background-color:gray;">
                       <div class="row d-flex align-items-center justify-content-center">
-                        <div class="col-9">
-                            <h3 class="text-center fw-bold">Administor Sign Up </h3>
-                            <div class="mt-4 pb-4"><input class="form-control" type="email" placeholder="email" id="email"/></div>
-                            <div class="pb-4"><input class="form-control" type="password" placeholder="passowrd" id="password"/></div>
-                            <div class="pb-4"><input class="form-control" type="password" placeholder="confirm-password" id="rePassword"/></div>
-                            <hr/>
+                        <div class="col-12">
+                            <h3 class="text-center fw-bold text-white"> Sign Up Page</h3>
+                            <div class="mt-4 pb-2"><input class="form-control" type="email" placeholder="email" id="email"/></div>
+                            <div class="pb-2"><input class="form-control" type="password" placeholder="passowrd" id="password"/></div>
+                            <div class="pb-2"><input class="form-control" type="password" placeholder="confirm-password" id="rePassword"/></div>
+                            <hr class="text-white"/>
                             <div class="d-grid pb-2"><button class="btn btn-primary" id="signUp">Sign Up</button></div>
                             <div class="d-grid"><button class="btn btn-primary" id="signIn">Sign In</button></div>
                         </div>
