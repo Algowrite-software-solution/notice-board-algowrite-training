6e0cd7107be8ace73b18d3b1ec2dcb8d41d3634b
kavindu202 <kavindu.sasanka962@gmail.com>
2023-06-10 00:00:45 +0530

    done 2

--- signUpPage.php
+++ signUpPage.php
@@ -14,10 +14,10 @@
         <div class="row">
             <div class="col-12 bg-black">
                 <div class="row signUpContainer">
-                    <div class="col-8 p-5 rounded" style="background-color:gray;">
+                    <div class="col-8 p-5 rounded-5" style="background-color:gray;">
                       <div class="row d-flex align-items-center justify-content-center">
-                        <div class="col-9">
-                            <h3 class="text-center fw-bold">Sign Up </h3>
+                        <div class="col-12">
+                            <h3 class="text-center fw-bold">Sign Up Page</h3>
                             <div class="mt-4 pb-2"><input class="form-control" type="email" placeholder="email"/></div>
                             <div class="pb-2"><input class="form-control" type="password" placeholder="passowrd"/></div>
                             <div class="pb-2"><input class="form-control" type="password" placeholder="confirm-password"/></div>
